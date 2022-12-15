73f852beda4cb09dfc7ed9af3712a72dc2b66c13
stevesun <sunguoqin@oneinfitech.com>
2022-12-15 06:08:19 -0800

    4

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,15 +1,16 @@
-import { prisma } from "~/db.server";
+import { prisma } from "../db.server";
+import type { Post } from "@prisma/client";
 
 export async function getPosts() {
   return prisma.post.findMany();
 }
 
-type Post = {
-    slug: string;
-    title: string;
-};
-
 export async function getPost(slug: string) {
-    return prisma.post.findUnique({ where: { slug } });
+  return prisma.post.findUnique({ where: { slug } });
 }
 
+export async function createPost(
+    post: Pick<Post, "slug" | "title" | "markdown">
+  ) {
+    return prisma.post.create({ data: post });
+  }
