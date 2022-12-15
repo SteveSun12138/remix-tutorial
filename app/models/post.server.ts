ed840010526378ba90fe16c57e6e0f1a82dd49c0
stevesun <sunguoqin@oneinfitech.com>
2022-12-15 05:37:53 -0800

    2

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,17 +1,14 @@
+import { prisma } from "~/db.server";
+
+export async function getPosts() {
+  return prisma.post.findMany();
+}
+
 type Post = {
     slug: string;
     title: string;
-  };
-  
-  export async function getPosts(): Promise<Array<Post>> {
-    return [
-      {
-        slug: "my-first-post",
-        title: "My First Post",
-      },
-      {
-        slug: "90s-mixtape",
-        title: "A Mixtape I Made Just For You",
-      },
-    ];
-  }
+};
+
+export async function getPost(slug: string) {
+    return prisma.post.findUnique({ where: { slug } });
+}
